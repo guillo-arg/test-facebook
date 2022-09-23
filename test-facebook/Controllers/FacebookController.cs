1b6861e5fb137256245158d7b41bca0e384869fe
guillermo amaya <guillermo_107@hotmail.com>
2022-09-23 11:28:49 -0300

    Add project files.

---
+++ test-facebook/Controllers/FacebookController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace test_facebook.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FacebookController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<FacebookController> _logger;
+
+        public FacebookController(ILogger<FacebookController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> get() 
+        {
+            var token = Request.Query["hub.verify_token"];
+            string response = Request.Query["hub.challenge"];
+            _logger.LogInformation($"QueryString: {Request.QueryString.Value}");
+            _logger.LogInformation($"Content-type: {Request.ContentType}");
+            _logger.LogInformation($"X-Forwarded-For: {Request.Headers["X-Forwarded-For"]}");
+
+            return Ok(response);
+
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(RequestDTO requestDTO)
+        {
+            _logger.LogInformation(JsonSerializer.Serialize(requestDTO));
+            return Ok();
+        }
+
+
+    }
+}
---
+++ test-facebook/RequestDTO.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test_facebook
+{
+
+    public class RequestDTO
+    {
+        public string @object { get; set; }
+        public Entry[] entry { get; set; }
+    }
+
+    public class Entry
+    {
+        public string id { get; set; }
+        public long time { get; set; }
+        public Messaging[] messaging { get; set; }
+        public Hop_Context[] hop_context { get; set; }
+        public Change[] changes { get; set; }
+    }
+
+    public class Messaging
+    {
+        public Sender sender { get; set; }
+        public Recipient recipient { get; set; }
+        public long timestamp { get; set; }
+        public Message message { get; set; }
+        public Postback postback { get; set; }
+    }
+
+    public class Sender
+    {
+        public string id { get; set; }
+    }
+
+    public class Recipient
+    {
+        public string id { get; set; }
+    }
+
+    public class Message
+    {
+        public string mid { get; set; }
+        public string text { get; set; }
+        public Attachment[] attachments { get; set; }
+    }
+
+    public class Hop_Context
+    {
+        public long app_id { get; set; }
+        public string metadata { get; set; }
+    }
+
+    public class Attachment
+    {
+        public string type { get; set; }
+        public Payload payload { get; set; }
+    }
+
+    public class Payload
+    {
+        public string url { get; set; }
+    }
+
+    public class Change
+    {
+        public Value value { get; set; }
+        public string field { get; set; }
+    }
+
+    public class Value
+    {
+        public From from { get; set; }
+        public string post_id { get; set; }
+        public int created_time { get; set; }
+        public string item { get; set; }
+        public string parent_id { get; set; }
+        public string reaction_type { get; set; }
+        public string verb { get; set; }
+        public Post post { get; set; }
+        public string message { get; set; }
+        public string comment_id { get; set; }
+    }
+
+    public class From
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Postback
+    {
+        public string title { get; set; }
+        public string payload { get; set; }
+    }
+
+    public class Post
+    {
+        public string status_type { get; set; }
+        public bool is_published { get; set; }
+        public string updated_time { get; set; }
+        public string permalink_url { get; set; }
+        public string promotion_status { get; set; }
+        public string id { get; set; }
+
+    }
+}
